d1dfe626aaf54eba7eab7443856219cbad09ba1b
AzamjonKobulov <azamjonkobulov1998@gmail.com>
2024-04-25 12:23:20 +0500

    add/notifications

--- public/js/script.js
+++ public/js/script.js
@@ -10,3 +10,25 @@ document.addEventListener('scroll', function () {
     header.classList.remove('-translate-y-8');
   }
 });
+
+// Notifications
+const notifications = document.querySelector('.notifications');
+const notificationsContent = document.querySelector('.notifications-content');
+const notificationsOpen = document.querySelector('.notifications-open');
+const notificationsClose = document.querySelector('.notifications-close');
+
+function toggleNotifications() {
+  notifications.classList.toggle('-translate-y-full');
+  notificationsContent.classList.toggle('-translate-y-full');
+  notificationsClose.classList.toggle('hidden');
+  document.body.classList.toggle('overflow-hidden');
+}
+
+notificationsOpen.addEventListener('click', toggleNotifications);
+notificationsClose.addEventListener('click', toggleNotifications);
+
+notifications.addEventListener('click', (event) => {
+  if (event.target === notifications) {
+    toggleNotifications();
+  }
+});
